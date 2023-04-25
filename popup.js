e115dd67c4541764db8b0dc095f5f18286dc2910
Nick Knighton <nickk@tamu.edu>
2023-04-25 14:56:53 -0500

    added ability to change css with dropdown

--- popup.js
+++ popup.js
@@ -1,10 +1,16 @@
-localStorage.clear();
-
 document.addEventListener('DOMContentLoaded', function() {
   var cssSelect = document.getElementById('css-select');
   var increaseButton = document.getElementById('increase-font');
   var decreaseButton = document.getElementById('decrease-font');
   var cssFileInput = document.getElementById('css-file');
+
+
+  cssSelect.addEventListener('change', function() {
+    var selectedCss = cssSelect.value;
+    chrome.tabs.executeScript({
+      code: 'var link = document.createElement("link"); link.rel = "stylesheet"; link.href = "' + selectedCss + '"; document.head.appendChild(link);'
+    });
+  });
 
   increaseButton.addEventListener('click', function() {
     chrome.tabs.executeScript({
