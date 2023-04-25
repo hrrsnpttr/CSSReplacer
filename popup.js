eb7b441ea4aadbb3d91e9e87d992057133b85cd7
harrisonsSchoolAcct <harrisonpotter2000@gmail.com>
2023-04-25 12:53:49 -0500

    Added all css sheets

--- popup.js
+++ popup.js
@@ -1,10 +1,21 @@
 localStorage.clear();
 
 document.addEventListener('DOMContentLoaded', function() {
-  var cssSelect = document.getElementById('css-select');
+  var cssSelect = document.getElementById('color-select');
   var increaseButton = document.getElementById('increase-font');
   var decreaseButton = document.getElementById('decrease-font');
   var cssFileInput = document.getElementById('css-file');
+  var selectedCssUrl = null;
+
+  function applyCSS() {
+    var selectedCss = cssSelect.value;
+    if (selectedCssUrl !== null) {
+      URL.revokeObjectURL(selectedCssUrl);
+    }
+    chrome.tabs.executeScript({
+      code: 'var link = document.createElement("link"); link.rel = "stylesheet"; link.href = "' + selectedCss + '"; document.head.appendChild(link);'
+    });
+  }
 
   increaseButton.addEventListener('click', function() {
     chrome.tabs.executeScript({
@@ -27,5 +38,10 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     };
     reader.readAsText(cssFileInput.files[0]);
+  });
+
+  cssSelect.addEventListener('change', function() {
+    applyCSS(); // call applyCSS()
+    // add the existing code that applies the CSS here as well
   });
 });
